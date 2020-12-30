2ec56b2e2972eff5b5da9e4805095f0cd31aef49
dpak1999 <deepakkumardash313@gmail.com>
2020-12-30 22:38:56 +0530

    add : global styles

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import PrimaryButton, {
   SecondaryButton,
   TertiaryButton,
 } from "./Components/Buttons";
+import { GlobalStyle } from "./utils";
 
 const App = () => (
   <div>
     <PrimaryButton>Hello World</PrimaryButton>
     <SecondaryButton>Goodbye World</SecondaryButton>
     <TertiaryButton>Hey World</TertiaryButton>
+    <GlobalStyle />
   </div>
 );
 
